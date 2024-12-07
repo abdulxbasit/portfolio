2497c5162ddc150f6db38bddd387d2f02b9fd809
abdulxbasit <abdulbasit.en@gmail.com>
2024-12-07 18:58:08 +0500

    change name to SS

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Abdul Basit</h1>
+      <h1>Salman Shahid</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
